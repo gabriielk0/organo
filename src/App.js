2538486452acd4820fa79a3ba5cc539b71aa4d03
Gabriel <oliveiracgabriel052@gmail.com>
2024-08-25 19:57:28 -0300

    Adicionando Card e escondendo componentes vazios

--- src/App.js
+++ src/App.js
@@ -60,7 +60,13 @@ function App() {
         }
       />
 
-      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>)}
+      {times.map(time => <Time 
+        key={time.nome} 
+        nome={time.nome} 
+        corPrimaria={time.corPrimaria} 
+        corSecundaria={time.corSecundaria}
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+        />)}
     </div>
   );
 }
--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,14 +1,23 @@
-import './Time.css'
+import Colaborador from "../Colaborador";
+import "./Time.css";
 
 const Time = (props) => {
-    
-    const css = {backgroundColor: props.corSecundaria}
+  const css = { backgroundColor: props.corSecundaria };
 
-    return (
-        <section className='time' style={css}>
-            <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
-        </section>
-    )
-}
+  return (
+    (props.colaboradores.length > 0) && <section className="time" style={css}>
+      <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+      <div className="colaboradores">
+        {props.colaboradores.map((colaborador) => (
+          <Colaborador
+            nome={colaborador.nome}
+            cargo={colaborador.cargo}
+            imagem={colaborador.imagem}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default Time
+export default Time;
