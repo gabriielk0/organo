ab0f7bdd6e2f27579b96f8b2efcab9f7943a3ab0
Gabriel <oliveiracgabriel052@gmail.com>
2024-08-25 16:51:10 -0300

    Adicionando a função de adicionar colaborador

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import Banner from './components/Banner';
-import Formulario from './components/Formulario';
+import { useState } from "react";
+import Banner from "./components/Banner";
+import Formulario from "./components/Formulario";
 
 function App() {
+  const [colaborador, setColaboradores] = useState([]);
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador);
+    setColaboradores([...setColaboradores, colaborador]);
+  };
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario
+        aoColaboradorCadastrado={(colaborador) =>
+          aoNovoColaboradorAdicionado(colaborador)
+        }
+      />
     </div>
   );
 }
