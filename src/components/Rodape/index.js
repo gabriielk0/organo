672169d0c00d69d7ff96f9a9b4b59c7fa303d369
gabriielk0 <oliveiracgabriel052@gmail.com>
2024-08-26 12:26:35 -0300

    Adicionando Rodapé

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,19 @@
+import './Rodape.css'
+
+const Rodape = () => {
+    return (
+        <footer className='rodape'>
+            <div>
+                <img src='/imagem/fb.png'/>
+                <img src='/imagem/tw.png'/>
+                <img src='/imagem/ig.png'/>
+            </div>
+            <img src='/imagem/logo.png'/>
+            <div>
+                <p>Desenvolvido por Alura.</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Rodape
