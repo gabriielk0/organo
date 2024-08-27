ef24475401e629b82ed96117a68c1e3061564c8f
gabriielk0 <oliveiracgabriel052@gmail.com>
2024-08-27 13:21:29 -0300

    update

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,23 +1,26 @@
 import Colaborador from "../Colaborador";
 import "./Time.css";
 
-const Time = (props) => {
+const Time = ( time, colaboradores, aoDeletar) => {
   const css = {backgroundImage: 'url(/images/fundo.png)' ,backgroundColor: props.corSecundaria };
 
   return (
-    props.colaboradores.length > 0 && (
+    colaboradores.length > 0 && (
       <section className="time" style={css}>
-        <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+        <input value={time.corSecundaria} type='color' className='input-cor' />
+        <h3 style={{ borderColor: time.corPrimaria }}>{time.nome}</h3>
         <div className="colaboradores">
-          {props.colaboradores.map((colaborador, indice) => (
-            <Colaborador
-              corDeFundo={props.corPrimaria}
-              key={indice}
-              nome={colaborador.nome}
-              cargo={colaborador.cargo}
-              imagem={colaborador.imagem}
-            />
-          ))}
+          {colaboradores.map((colaborador, indice) => {
+            console.log('renderizando o colaborador');
+            return <Colaborador
+            key={indice}
+            nome={colaborador.nome}
+            cargo={colaborador.cargo}
+            imagem={colaborador.imagem}
+            corDeFundo={time.corPrimaria}
+            aoDeletar={(aoDeletar) => {}}
+          />
+          })}
         </div>
       </section>
     )
